2ca7800ac9f5df5771b949814de2eef5a4cf423d
Abhay Amin <contactabhay2@gmail.com>
2025-11-27 18:48:16 +0530

    removed lock files

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,6 @@ import { ConfigModule } from "@nestjs/config";
 import { ContactModule } from "@/modules/contacts/contact.module";
 import { DatabaseModule } from "@/core/database/database.module";
 import { AwardsModule } from "./modules/awards/awards.module";
-import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), ContactModule, DatabaseModule, AwardsModule],
