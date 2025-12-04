cac36afcdbde56119d7eeb2b4cb19763bb2f163b
sai-edu-admin <webcms@sai.edu.in>
2025-12-04 19:45:27 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "@/app.module";
 import { ResponseInterceptor } from "@/common/interceptors/response.interceptor";
-import { ResponseInterceptor } from "@/common/interceptors/response.interceptor";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
