270700f96ac33f2b04812a26e16c614eb953e2c1
sai-edu-admin <webcms@sai.edu.in>
2025-12-04 19:47:37 +0530

    Update press-releases.module.ts

--- src/modules/press-releases/press-releases.module.ts
+++ src/modules/press-releases/press-releases.module.ts
@@ -1,6 +1,10 @@
-// MODULES //
+// CONTROLLERS //
 import { PressReleasesController } from "../../modules/press-releases/press-releases.controller";
+
+// SERVICES //
 import { PressReleasesService } from "../../modules/press-releases/press-releases.service";
+
+// MODULES //
 import { DatabaseModule } from "../../core/database/database.module";
 
 // OTHERS //
