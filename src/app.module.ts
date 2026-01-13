cc3d8862d9b031f7ef69e32cd8df0820b3f6db10
sai-edu-admin <webcms@sai.edu.in>
2026-01-13 17:01:26 +0530

    added student council

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { PressReleasesModule } from "./modules/press-releases/press-releases.mod
 import { SioneersModule } from "./modules/sioneers/sioneers.module";
 import { ResultsModule } from "./modules/results/results.module";
 import { BlogsModule } from "./modules/blogs/blogs.module";
+import { StudentCouncilModule } from "./modules/student-council/student-council.module";
 
 // CONTROLLERS //
 import { AppController } from "./app.controller";
@@ -29,6 +30,7 @@ import { ConfigModule } from "@nestjs/config";
     SioneersModule,
     ResultsModule,
     BlogsModule,
+    StudentCouncilModule,
   ],
   controllers: [AppController],
   providers: [AppService],
