b6b3fb1c81ee5503052802626917b264087c9165
Abhay Amin <contactabhay2@gmail.com>
2026-01-17 20:39:57 +0530

    Adding Cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ResponseInterceptor } from "./common/interceptors/response.interceptor";
+import { ExpressAdapter } from "@nestjs/platform-express";
+import express from "express";
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   // Response Interceptor (to format data)
   app.useGlobalInterceptors(new ResponseInterceptor());
