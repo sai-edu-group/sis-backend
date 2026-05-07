fda42d582bf09748e4c0bc816de254e9b97401d9
jayants1903 <jayantsinghhh89@gmail.com>
2026-05-07 11:10:50 +0530

    comments added for sessions API

--- src/modules/sessions/sessions.controller.ts
+++ src/modules/sessions/sessions.controller.ts
@@ -6,6 +6,15 @@ import { SessionsService } from "./sessions.service";
 export class SessionsController {
   constructor(private readonly sessionsService: SessionsService) {}
 
+  /**
+   * GET: /sessions?scope={scope}
+   *
+   * Fetch sessions based on the provided scope. Supported scopes are:
+   * - `awards`: Returns sessions that have associated awards.
+   * - `results`: Returns sessions that have associated results.
+   *
+   * If the scope is not provided or is unsupported, a BadRequestException is thrown.
+    */
   @Get()
   getSessions(@Query("scope") scope?: string) {
     if (!scope) {
