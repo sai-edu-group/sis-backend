30102b76c128d23efd08ab754832f171a9006b23
jayants1903 <jayantsinghhh89@gmail.com>
2026-05-15 12:00:22 +0530

    Env fix in db.ts

--- src/core/database/db.ts
+++ src/core/database/db.ts
@@ -9,7 +9,7 @@ console.log("DATABASE SE CONNECT HO");
 
 console.log(process.env.DB_HOST);
 console.log(process.env.DB_USER);
-console.log(process.env.DB_PASSWORD);
+console.log(process.env.DB_PASS);
 console.log(process.env.DB_NAME);
 console.log(process.env.DB_PORT);
 
@@ -17,7 +17,7 @@ const dialect = new MysqlDialect({
   pool: createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     port: Number(process.env.DB_PORT || 3306),
     connectionLimit: 10,
